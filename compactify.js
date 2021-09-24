9c1fc44a1ad15b8b336dfac6702b390f00427644
Paul THOMAS <paulthomas.dn@gmail.com>
2021-09-24 14:13:20 -0400

    Feature: added icon for toggling visibility

--- compactify.js
+++ compactify.js
@@ -4,6 +4,10 @@ let selectors = [
     '[class^=sidebar]'
 ]
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function doCompact() {
     isCompact = !isCompact
     let visibility = isCompact ? "collapse" : "visible"
@@ -12,7 +16,36 @@ function doCompact() {
     )
 }
 
+async function addIcon() {
+    let element
+    while (true) {
+        await sleep(100)
+        if (document == null) continue
+        element = document.querySelector('[class^="chat"]')
+        if (element == null) continue
+        element = element.querySelector('[class^=children]')
+        if (element == null) continue
+        break
+    }
+
+    if (element.parentNode.firstChild.id != "compact_icon") {
+        fetch(browser.runtime.getURL("/icon.html"))
+            .then(r => {
+                r.text()
+                    .then(html => {
+                        element.insertAdjacentHTML('beforebegin', html)
+                        element.parentNode.firstChild.addEventListener("click", () => doCompact())
+                    })
+            })
+        return true
+    } else {
+        return false
+    }
+}
+
 browser.runtime.onMessage.addListener(_ => {
     doCompact()
     return Promise.resolve({ response: "OK" })
 })
+
+addIcon()
